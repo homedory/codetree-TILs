80b9e71ddd1bc67fdc8c651438420e4a92d54be2
homedory <77291306+homedory@users.noreply.github.com>
2024-01-23 00:18:06 +0900

    Title: 수열 조작, Time: 11ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 240123/수열 조작/sequence-manipulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    deque <int> dq;
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        dq.push_back(i);
+    }
+    while(dq.size() > 1) {
+        dq.pop_front();
+        dq.push_back(dq.front());
+        dq.pop_front();
+    }
+
+    cout << dq.front();
+    return 0;
+}

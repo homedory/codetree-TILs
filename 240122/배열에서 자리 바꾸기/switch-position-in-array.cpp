c5a20e01387b3037226e767389f86a534258229c
homedory <77291306+homedory@users.noreply.github.com>
2024-01-22 19:35:08 +0900

    Title: 배열에서 자리 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/배열에서 자리 바꾸기/switch-position-in-array.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+#define MAX_N 250005
+
+struct Node {
+    Node *prev, *next;
+    int data;
+
+    Node(int data) : data(data), prev(nullptr), next(nullptr) {}
+};
+
+void connect(Node *s, Node *u) {
+    if (s != nullptr)
+        s->next = u;
+    if (u != nullptr)
+        u->prev = s;
+}
+
+void PopAndInsertPrev(Node *s, Node *e, Node *v) {
+    connect(s->prev, e->next);
+    s->prev = e->next = nullptr;
+
+    connect(v->prev, s);
+    connect(e, v);
+}
+
+void PopAndInsertNext(Node *s, Node *e, Node *v) {
+    connect(s->prev, e->next);
+    s->prev = e->next = nullptr;
+
+    connect(e, v->next);
+    connect(v, s);
+}
+
+void PopAndSwap(Node *a, Node *b, Node *c, Node *d) {
+    Node *a_prev = a->prev;
+    PopAndInsertPrev(a, b, d->next);
+    PopAndInsertNext(c, d, a_prev);
+}
+
+
+int main() {
+    int N, Q;
+    Node* nodes[MAX_N];
+
+    cin >> N;
+    cin >> Q;
+
+    nodes[0] = new Node(0);
+    for (int i = 1; i <= N + 1; i++) {
+        Node *node = new Node(i);
+        nodes[i] = node;
+        connect(nodes[i-1], node);
+    }
+
+    for (int i = 0; i < Q; i++) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        PopAndSwap(nodes[a], nodes[b], nodes[c], nodes[d]);
+    }
+
+
+    Node *ptr = nodes[1];
+    while(ptr != nullptr && ptr->data != N + 1) {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+
+    return 0;
+}

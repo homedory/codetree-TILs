6d063878cade04c3bc7416889cca98fd95e3c73f
homedory <77291306+homedory@users.noreply.github.com>
2024-01-26 01:15:41 +0900

    Title: 점 개수 세기 2, Time: 340ms, Memory: 6MB, Status: Passed - CodeTree

--- 240126/점 개수 세기 2/count-number-of-points-2.cpp
+++ 240126/점 개수 세기 2/count-number-of-points-2.cpp
@@ -55,6 +55,11 @@ int main() {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
+        if (x_set.lower_bound(x1) == x_set.upper_bound(x2) || y_set.lower_bound(y1) == y_set.upper_bound(y2)) {
+            cout << "0\n";
+            continue;
+        }
+
         int nx1 = x_mapper[*x_set.lower_bound(x1)];
         int ny1 = y_mapper[*y_set.lower_bound(y1)];
 

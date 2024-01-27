d1c146060e51b810c1eb73304f6fa0e8613f9a68
homedory <77291306+homedory@users.noreply.github.com>
2024-01-27 18:50:11 +0900

    Title: 겹치지 않는 선분, Time: 88ms, Memory: 1MB, Status: Passed - CodeTree

--- 240127/겹치지 않는 선분/line-segments-that-do-not-overlap.cpp
+++ 240127/겹치지 않는 선분/line-segments-that-do-not-overlap.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
-#include <map>
+#include <tuple>
 #include <algorithm>
 using namespace std;
 
 #define MAX_N 100005
 
+
 int main() {
     int N;
-    map <int,int> line;
-    int arr[MAX_N];
-    
+    pair<int,int> line[MAX_N];
+    int L[MAX_N], R[MAX_N];
+
     cin >> N;
 
     for (int i = 0; i < N; i++) {
         int x1, x2;
         cin >> x1 >> x2;
-        line[x1] = x2;
-        arr[i] = x2;
+        line[i].first = x1;
+        line[i].second = x2;
     }
 
-    sort(arr, arr+N);
+    sort(line, line + N);
 
-    int cnt = 0, ans = 0;
-    for (map<int,int>::iterator it = line.begin(); it != line.end(); it++) {
-        int order = lower_bound(arr, arr + N, it->second) - arr;
-        if (order < cnt)
-            ans += cnt - order + 1;
-        cnt++;
+    int max_num = -1000000;
+    for (int i = 0; i < N; i++) {
+        max_num = max(max_num, line[i].second);
+        L[i] = max_num;
     }
 
+    int min_num = 1000000;
+    for (int i = N - 1; i >= 0; i--) {
+        min_num = min(min_num, line[i].second);
+        R[i] = min_num;
+    }
+
+    int ans = 0;
+
+    if (N == 1) {
+        cout << 1;
+        return 0;
+    }
+
+    if (line[0].second < R[1])
+        ans++;
+
+    if (line[N - 1].second > L[N - 2])
+        ans++;
+    
+    for (int i = 1; i < N - 1; i++) {
+        if (L[i - 1] < line[i].second && line[i].second < R[i + 1])
+            ans++;
+    }
+    
     cout << ans;
 
-
+    
     return 0;
 }

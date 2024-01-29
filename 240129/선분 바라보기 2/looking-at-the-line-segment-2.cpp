e95d4e95a69698f26e56ee122035a2f3b1efec61
homedory <77291306+homedory@users.noreply.github.com>
2024-01-29 16:06:38 +0900

    Title: 선분 바라보기 2, Time: 148ms, Memory: 2MB, Status: Passed - CodeTree

--- 240129/선분 바라보기 2/looking-at-the-line-segment-2.cpp
+++ 240129/선분 바라보기 2/looking-at-the-line-segment-2.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
-#include <set>
+#include <map>
 #include <vector>
 #include <tuple>
 #include <algorithm>
 using namespace std;
 
+#define MAX_N 50005
+
+int check[MAX_N];
+
 int main() {
     int n;
-    vector <tuple<int,int,int>> points;
+    vector <tuple<int,int,int,int>> points;
 
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         int x1, x2, y;
         cin >> y >> x1 >> x2;
-        points.push_back(make_tuple(x1, y, 1));
-        points.push_back(make_tuple(x2, y, -1));
+        points.push_back(make_tuple(x1, y, i, 1));
+        points.push_back(make_tuple(x2, y, i, -1));
     }
 
     sort(points.begin(), points.end());
 
-    set <int> s;
+    map <int, int> m;
 
     int front_y = -2000000000;
     int ans = 0;
 
     for (int i = 0; i < points.size(); i++) {
-        int x, y, v;
-        tie(x, y, v) = points[i];
+        int x, y, v, idx;
+        tie(x, y, idx, v) = points[i];
         if (v == 1) {
-            s.insert(y);
+            m[y] = idx;
         }
         else if (v == -1) {
-            s.erase(y);
+            m.erase(y);
         }
 
-        if (s.size() > 0 && *s.begin() != front_y) {
-            ans++;
-            front_y = *s.begin();
+        if (m.size() > 0 && m.begin()->first != front_y) {
+            if (!check[m.begin()->second]) {
+                ans++;
+                check[m.begin()->second] = 1;
+            }
+            front_y = m.begin()->first;
         }
     }
 

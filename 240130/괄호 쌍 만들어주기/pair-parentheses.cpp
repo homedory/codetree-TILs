6a20d580a883fd0e5704ebe14aaeb256468c1a69
homedory <77291306+homedory@users.noreply.github.com>
2024-01-30 12:05:07 +0900

    Title: 괄호 쌍 만들어주기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAX_N 100005
+
+char arr[MAX_N];
+int L[MAX_N], R[MAX_N];
+
+int main() {
+    string str;
+
+    cin >> str;
+
+    int n = str.length();
+
+    R[n - 1] = 0;
+    for (int i = n - 2; i >= 0; i--) {
+        if (str[i+1] == ')' && str[i] == ')') {
+            R[i] = R[i+1] + 1;
+        }
+        else {
+            R[i] = R[i+1];
+        }
+    }
+
+    int ans = 0;
+    
+    for (int i = 1; i < n - 2; i++) {
+        if (str[i-1] == '(' && str[i] == '(') {
+            ans += R[i + 1];
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}

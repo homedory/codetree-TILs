18f2d5678fa6f8729f5a47d6685a11017c97a77c
homedory <77291306+homedory@users.noreply.github.com>
2024-02-23 02:17:35 +0900

    Title: 숫자의 개수, Time: 236ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/숫자의 개수/number-of-integers.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 100005
+
+int n, m;
+int arr[MAX_N];
+
+int upper_bound(int x) {
+    int l = 1, r = n;
+    int min_idx = n + 1;
+
+    while(l <= r) {
+        int mid = (l + r) / 2;
+    
+        if (arr[mid] > x) {
+            r = mid - 1;
+            min_idx = min(min_idx, mid);
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    return min_idx;
+}
+
+int lower_bound(int x) {
+    int l = 1, r = n;
+    int min_idx = n + 1;
+
+    while(l <= r) {
+        int mid = (l + r) / 2;
+
+        if (arr[mid] >= x) {
+            r = mid - 1;
+            min_idx = min(min_idx, mid);
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    return min_idx;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+
+        cout << upper_bound(x) - lower_bound(x) << "\n";
+    }
+
+    return 0;
+}

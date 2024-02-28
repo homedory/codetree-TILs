364c41a58b3d9bd90365a901d66c337723499359
homedory <77291306+homedory@users.noreply.github.com>
2024-02-28 16:03:42 +0900

    Title: 부분문자열이 되는 횟수 구하기, Time: 34ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240228/부분문자열이 되는 횟수 구하기/find-the-number-of-partial-strings.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAX_N 200005
+
+int N;
+int order[MAX_N];
+string A, B;
+
+bool exist[MAX_N];
+
+void init() {
+    for (int i = 0; i < N; i++) {
+        exist[i] = true;
+    }
+}
+
+bool IsPossible(int x) {
+    init();
+
+    for (int i = 0; i < x; i++) {
+        exist[order[i] - 1] = false;
+    }
+
+    int j = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (exist[i] && A[i] == B[j]) {
+            j++;
+        }
+
+        if (j == B.length()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> A;
+    cin >> B;
+
+    N = A.length();
+
+    for (int i = 0; i < N; i++) {
+        cin >> order[i];
+    }
+
+    int l = 0, r = N;
+    int ans = 0;
+
+    while(l <= r) {
+        int mid = (l + r) / 2;
+
+        if (IsPossible(mid)) {
+            l = mid + 1;
+            ans = mid;
+        }
+        else {
+            r = mid - 1;
+        }
+    }
+
+    cout << ans + 1;
+    
+    return 0;
+}

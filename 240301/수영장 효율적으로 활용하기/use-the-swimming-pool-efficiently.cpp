d6b0b7473c1836d702887861a05e2a2e08165190
homedory <77291306+homedory@users.noreply.github.com>
2024-03-01 19:09:41 +0900

    Title: 수영장 효율적으로 활용하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+#define MAX_N 100005
+
+int n, m;
+int T[MAX_N];
+
+bool IsPossible(int t) {
+    int cnt = 1;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (sum + T[i] <= t) {
+            sum += T[i];
+        }
+        else {
+            cnt++;
+            sum = T[i];
+        }
+    }
+
+    return cnt <= m;
+}
+
+int main() {
+    cin >> n >> m;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> T[i];
+    }
+
+    int l = 1, r = INT_MAX;
+    int ans;
+    while(l <= r) {
+        int mid = (l + r) / 2;
+
+        if (IsPossible(mid)) {
+            r = mid - 1;
+            ans = mid;
+        }
+        else {
+            l = mid + 1;
+        }
+    } 
+
+    cout << ans;
+
+
+    return 0;
+}

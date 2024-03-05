09c96f04311fe97b256394cfab5c3205eed0af58
homedory <77291306+homedory@users.noreply.github.com>
2024-03-06 00:45:29 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 38ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240306/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define MAX_N 100005
+#define MAX_S 500005
+
+int n;
+pair<int,int> s[MAX_N];
+long long ans = 0;
+char arr[MAX_S];
+
+bool cmp(pair<int,int> a, pair<int,int> b) {
+    long long sum1 = (long long) a.first * b.second;
+    long long sum2 = (long long) b.first * a.second;
+
+    return sum1 >= sum2;
+}
+
+int main() {
+    
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr;
+
+        string str = arr;
+
+        int open = 0, close = 0;
+        for (int j = 0; j < (int) str.length(); j++) {
+            if (str[j] == '(') {
+                open++;
+            }
+            else {
+                close++;
+                ans += open;
+            }
+        }
+        s[i] = make_pair(open, close);
+    }
+
+    sort(s, s + n, cmp);
+
+    int open_sum = 0;
+    for (int i = 0; i < n; i++) {
+        int open = s[i].first;
+        int close = s[i].second;
+
+        ans += (long long) open_sum * close;
+
+        open_sum += open;
+    }
+
+    cout << ans;
+
+    return 0;
+}

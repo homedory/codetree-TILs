7358214d223160192205ccc33e8201ac944576f4
homedory <77291306+homedory@users.noreply.github.com>
2024-03-05 23:33:18 +0900

    Title: 좌우 반전시키기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240305/좌우 반전시키기/flip-left-and-right.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[105];
+
+int reverse(int n) {
+    return (n == 0) ? 1 : 0;
+}
+
+void flip(int i) {
+    arr[i - 1] = reverse(arr[i - 1]);
+    arr[i] = reverse(arr[i]);
+
+    if (i < n - 1) {
+        arr[i + 1] = reverse(arr[i + 1]);
+    }
+}
+
+int main() {   
+    int ans = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] == 0) {
+            flip(i + 1);
+            ans++;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}

3a7b88c742880874416f0238bd367765d8c38285
homedory <77291306+homedory@users.noreply.github.com>
2024-03-10 00:26:02 +0900

    Title: 최단 거리 3, Time: 42ms, Memory: 3MB, Status: Wrong Answer - CodeTree

--- 240310/최단 거리 3/shortest-distance-3.cpp
+++ 240310/최단 거리 3/shortest-distance-3.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 using namespace std;
 
 #define MAX_N 1005
 #define INF 2e9
 
+int edge[MAX_N][MAX_N];
+bool visited[MAX_N];
+
 int main() {
     int n, m;
     int A, B;
-    vector <pair<int,int>> edges[MAX_N];
     int dist[MAX_N];
 
     cin >> n >> m;
+
     for (int i = 0; i < m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        edges[a].push_back(make_pair(b, c));
-        edges[b].push_back(make_pair(a, c));
+        edge[a][b] = c;
+        edge[b][a] = c;
     }
 
     cin >> A >> B;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i <= n; i++) {
         dist[i] = INF;
     }
 
     dist[A] = 0;
 
-    priority_queue<pair<int,int>> pq;
+    for (int i = 1; i <= n; i++) {
+        int min_idx = 0;
+        for (int j = 1; j <= n; j++) {
+            if (visited[j])
+                continue;
 
-    pq.push(make_pair(0, A));
-
-    while(!pq.empty()) {
-        int current_dist = -pq.top().first;
-        int current_vertex = pq.top().second;
-        pq.pop();
-
-        if (dist[current_vertex] != current_dist)
-            continue;
-
-        for (int i = 0; i < edges[current_vertex].size(); i++) {
-            int next_vertex = edges[current_vertex][i].first;
-            int new_dist = current_dist + edges[current_vertex][i].second;
-
-            if (dist[next_vertex] > new_dist) {
-                dist[next_vertex] = new_dist;
-                pq.push(make_pair(-new_dist, next_vertex));
+            if (dist[j] < dist[min_idx]) {
+                min_idx = j;
             }
+        }
+
+        visited[min_idx] = true;
+
+        for (int j = 1; j <= n; j++) {
+            if (edge[min_idx][j] == 0)
+                continue;
+            
+            if (dist[j] > dist[min_idx] + edge[min_idx][j])
+                dist[j] = dist[min_idx] + edge[min_idx][j];
         }
     }
 

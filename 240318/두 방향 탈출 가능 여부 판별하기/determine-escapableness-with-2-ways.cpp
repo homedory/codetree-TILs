11e0eaec47c14345dc0e906e3682f900900e5b74
homedory <77291306+homedory@users.noreply.github.com>
2024-03-18 23:53:58 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 105
+
+bool visited[MAX_N][MAX_N];
+int board[MAX_N][MAX_N];
+int n, m;
+
+bool canGo(int x, int y) {
+    if (x > 0 && x <= n && y > 0 && y <= m && board[x][y] == 1) 
+        return true;
+    
+    return false;
+}
+
+void dfs(int x, int y) {
+    int dx[2] = {1, 0};
+    int dy[2] = {0, 1};
+
+    for (int i = 0; i < 2; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (canGo(nx, ny) && !visited[nx][ny]) {
+            visited[nx][ny] = true;
+            dfs(nx, ny);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    visited[1][1] = true;
+    dfs(1, 1);
+
+    if (visited[n][m])
+        cout << 1;
+    else
+        cout << 0;
+        
+    return 0;
+}

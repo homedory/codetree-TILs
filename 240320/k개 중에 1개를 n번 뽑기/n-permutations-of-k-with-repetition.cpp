21615be4231deae05c9cb3e950845240ee3a2422
homedory <77291306+homedory@users.noreply.github.com>
2024-03-20 23:28:20 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, K;
+vector <int> nums;
+
+void print() {
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << "\n";
+}
+
+void chooseNumber(int curr_num) {
+    if (curr_num == N + 1) {
+        print();
+        return;
+    }
+
+    for (int i = 1; i <= K; i++) {
+        nums.push_back(i);
+        chooseNumber(curr_num + 1);
+        nums.pop_back();
+    }
+    
+}
+
+int main() {
+    cin >> N >> K;
+
+    chooseNumber(1);
+
+    return 0;
+}

dc267abb52818263b79374f358e4010f25f02b42
homedory <77291306+homedory@users.noreply.github.com>
2024-04-03 00:33:27 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 598ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240403/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+
+unordered_set<int> nums;
+queue<pair<int,int>> num_q;
+
+bool isDuplicated(int num) {
+    return nums.find(num) != nums.end();
+}
+
+void addNum(int num, int cnt) {
+    if (isDuplicated(num)) 
+        return;
+    
+    num_q.push(make_pair(num, cnt));
+    nums.insert(num);
+}
+
+int findMinCalcCnt(int target) {
+    int min_cnt;
+
+    addNum(1, 0);
+    while(!num_q.empty()) {
+        auto [curr_num, curr_cnt] = num_q.front();
+        num_q.pop();
+
+        if (curr_num == target) {
+            min_cnt = curr_cnt;
+            break;
+        }
+
+        addNum(curr_num - 1, curr_cnt + 1);
+
+        if (curr_num > target)
+            continue;
+        
+        addNum(curr_num + 1, curr_cnt + 1);
+        addNum(curr_num * 2, curr_cnt + 1);
+        addNum(curr_num * 3, curr_cnt + 1);
+    }
+
+    return min_cnt;
+}
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    cout << findMinCalcCnt(N);
+
+    return 0;
+}

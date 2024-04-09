1453d9ed9f1ca0fcc67b382e2b7fda9274b0f0e0
homedory <77291306+homedory@users.noreply.github.com>
2024-04-10 00:53:57 +0900

    Title: 사각형 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240410/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 1005
+
+int dp[MAX_N];
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= N; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[N];
+    
+    return 0;
+}

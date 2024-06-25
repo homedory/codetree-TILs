47f483a22d85e1d8988186d8598a94c2348f1bab
homedory <77291306+homedory@users.noreply.github.com>
2024-06-25 19:58:16 +0900

    Title: 금 채굴하기, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240625/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int grid[25][25];
+
+
+int findMax(int x, int y) {
+    int max_cnt = 0;
+
+    for (int k = 1; k <= n; k++) {
+        int l = k / 2;
+        int gold_count = 0;
+
+        for (int i = -l; i <= l; i++) {
+            int nx = x + i;
+            if (nx < 0 || nx > n)
+                continue;
+            
+            for (int j = -(k - 1); j <= (k - l); j++) {
+                int ny = y + j;
+                if (ny < 0 || ny > n)
+                    continue;
+                
+                if (grid[nx][ny] == 1)
+                    gold_count++;
+            }
+        }
+
+        if (k*k + (k+1)*(k+1) > m * gold_count)
+            continue;
+        
+        max_cnt = max(max_cnt, gold_count);
+
+    }
+    
+    return max_cnt;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int cnt = findMax(i, j);
+            ans = max(ans, cnt);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}

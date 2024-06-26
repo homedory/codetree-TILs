465c7c5e9f140d073391967b6dfa7f5b861a8f0f
homedory <77291306+homedory@users.noreply.github.com>
2024-06-26 23:44:43 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240626/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int grid[5][5];
+int n, m;
+
+int calcSum(int left, int right, int bottom, int top) {
+    int sum = 0;
+    for (int i = left; i <= right; i++) {
+        for (int j = bottom; j <= top; j++) {
+            sum += grid[i][j];
+        }
+    }
+
+    return sum;
+}
+
+int selectColumn(int bottom, int top) {
+    int max_sum = INT_MIN;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = i; j < m; j++) {
+            max_sum = max(max_sum, calcSum(i, j, bottom, top));
+        }
+    }
+    return max_sum;
+}
+
+int selectRow(int left, int right) {
+    int max_sum = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            max_sum = max(max_sum, calcSum(left, right, i, j));
+        }
+    }
+    return max_sum;
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int ans = INT_MIN;
+
+    for (int bottom_1 = 0; bottom_1 < n - 1; bottom_1++) {
+        for (int top_1 = bottom_1; top_1 < n - 1; top_1++) {
+            for (int bottom_2 = top_1 + 1; bottom_2 < n; bottom_2++) {
+                for (int top_2 = bottom_2; top_2 < n; top_2++) {
+                    int sum = selectColumn(bottom_1, top_1) + selectColumn(bottom_2, top_2);
+                    ans = max(ans, sum);
+                }
+            }
+        }
+    }
+
+    for (int left_1 = 0; left_1 < m - 1; left_1++) {
+        for (int right_1 = left_1; right_1 < m - 1; right_1++) {
+            for (int left_2 = right_1 + 1; left_2 < m; left_2++) {
+                for (int right_2 = left_2; right_2 < m; right_2++) {
+                    int sum = selectRow(left_1, right_1) + selectRow(left_2, right_2);
+                    ans = max(ans, sum);
+                }
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}

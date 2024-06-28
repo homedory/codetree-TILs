ecdc07dff712d3715c534034b15507ebe0d87938
homedory <77291306+homedory@users.noreply.github.com>
2024-06-28 22:54:27 +0900

    Title: 행렬로 주어진 간선, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240628/행렬로 주어진 간선/edge-given-by-matrix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int edge[105][105];
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> edge[i][j];
+        }
+    }
+
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n ; j++) {
+                if (edge[i][k] && edge[k][j])
+                    edge[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << edge[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}

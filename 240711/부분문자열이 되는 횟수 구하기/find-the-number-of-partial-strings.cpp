232868f22fa9e8c09685d13b869d6afcfefcb3b8
homedory <77291306+homedory@users.noreply.github.com>
2024-07-11 23:38:20 +0900

    Title: 부분문자열이 되는 횟수 구하기, Time: 45ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240711/부분문자열이 되는 횟수 구하기/find-the-number-of-partial-strings.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int A_length;
+vector<int> erase_order;
+string A, B;
+
+bool isPartialString(int erase_num) {
+    unordered_set <int> erased;
+    for (int i = 0; i < erase_num; i++) {
+        erased.insert(erase_order[i]);
+    }
+
+    int index = 0;
+    for (int i = 0; i < A_length; i++) {
+        if (erased.find(i+1) != erased.end())
+            continue;
+        
+        if (A[i] == B[index])
+            index++;
+        
+        if (index == B.length()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> A;
+    cin >> B;
+    A_length = A.length();
+
+    for (int i = 0; i < A_length; i++) {
+        int num;
+        cin >> num;
+        erase_order.push_back(num);
+    }
+
+
+    int left = 0;
+    int right = A_length;
+    int ans = 0;
+    
+    while(left <= right) {
+        int mid = (left + right) / 2;
+
+        if (isPartialString(mid)) {
+            left = mid + 1;
+            ans = mid;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    ans++;
+
+    cout << ans;
+
+    return 0;
+}

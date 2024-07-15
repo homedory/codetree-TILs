d8cce0d65ab451c7e7e88516d4adcca4b58899be
homedory <77291306+homedory@users.noreply.github.com>
2024-07-15 23:28:54 +0900

    Title: 트리 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/트리 판별/tree-identification.cpp
+++ 240715/트리 판별/tree-identification.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <unordered_set>
+#include <vector>
 #include <unordered_map>
+#include <queue>
+
 using namespace std;
 
 int main() {
     int m;
     unordered_map <int,int> in_degree;
-    unordered_set <int> node;
+    vector <int> edges[10005];
     bool ans = true;
     
     cin >> m;
@@ -14,6 +16,8 @@ int main() {
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
+        edges[a].push_back(b);
+
         if (in_degree.find(b) != in_degree.end() && in_degree[b] > 0) {
             ans = false;
         }
@@ -27,8 +31,45 @@ int main() {
     if (in_degree.size() != m + 1)
         ans = false;
     
+    if (ans == false) {
+        cout << ans;
+        return 0;
+    }
+
+    int root;
+    for(unordered_map<int,int>::iterator it = in_degree.begin(); it != in_degree.end(); it++) {
+        if(it->second == 0)
+            root = it->first;
+    }
+
+    int connected_node_cnt = 0;
+    queue <int> q;
+    bool visited[10005];
+    for (int i = 0; i < 10005; i++)
+        visited[i] = false;
+
+    q.push(root);
+    visited[root] = true;
+    while(!q.empty()) {
+        connected_node_cnt++;
+        int cur_node = q.front();
+        q.pop();
+
+        for (int i = 0; i < edges[cur_node].size(); i++) {
+            int next_node = edges[cur_node][i];
+            if (visited[next_node])
+                continue;
+            
+            q.push(next_node);
+            visited[next_node] = true;
+        }
+    }
+
+    if (connected_node_cnt != m + 1)
+        ans = false;
+
     cout << ans;
 
-    
+
     return 0;
 }

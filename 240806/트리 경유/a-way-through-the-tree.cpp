15078baf2389afe0d003f1483f943ccad10011cc
homedory <77291306+homedory@users.noreply.github.com>
2024-08-06 23:08:39 +0900

    Title: 트리 경유, Time: 519ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240806/트리 경유/a-way-through-the-tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+unordered_set<int> checked_node;
+
+int getBlockingNode(int start_node) {
+    int node = start_node;
+    int blocking_node = 1;
+    bool blocked = false;
+    while(node > 1) {
+        if (checked_node.find(node) != checked_node.end()) {
+            blocking_node = node;
+            blocked = true;
+        }
+        node /= 2;
+    }
+
+    return blocking_node;
+}
+
+int main() {
+    
+    int n, q;
+
+    cin >> n >> q;
+    for (int i = 0; i < q; i++) {
+        int node;
+        cin >> node;
+
+        int blocking_node = getBlockingNode(node);
+        if (blocking_node == 1) {
+            checked_node.insert(node);
+            cout << 0 << "\n";
+        }
+        else {
+            cout << blocking_node << "\n";
+        }
+    }
+
+
+    return 0;
+}

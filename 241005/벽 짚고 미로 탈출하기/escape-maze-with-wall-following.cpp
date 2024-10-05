4f453e4c43689ea190db992bc46dc9d32083cead
homedory <77291306+homedory@users.noreply.github.com>
2024-10-05 22:16:28 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 1030ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241005/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 205
+
+int N, cur_x, cur_y;
+char grid[MAX_N][MAX_N];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+int turnLeft(int dir) {
+    return ((dir == 0) ? 3: dir-1);
+}
+
+int turnRigiht(int dir) {
+    return (dir + 1) % 4;
+}
+
+bool canMove(int dir) {
+    int next_x = cur_x + dx[dir];
+    int next_y = cur_y + dy[dir];
+
+    if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= N)
+        return true;
+    else if (grid[next_x][next_y] == '.')
+        return true;
+    else
+         return false;
+}
+
+void move(int dir) {
+    cur_x += dx[dir];
+    cur_y += dy[dir];
+}
+
+bool hasEnded() {
+    if (cur_x < 0 || cur_x >= N || cur_y < 0 || cur_y >= N)
+        return true;
+    
+    return false;
+}
+
+bool rightWallExists(int dir) {
+    int right_dir = turnRigiht(dir);
+    int right_x = cur_x + dx[right_dir];
+    int right_y = cur_y + dy[right_dir];
+
+    return grid[right_x][right_y] == '#';
+}
+
+int main() {
+    cin >> N;
+    cin >> cur_x >> cur_y;
+    for (int i = 0; i < N; i++) {
+        cin >> grid[i];
+    }
+
+    cur_x -= 1;
+    cur_y -= 1;
+    int total_time = 0;
+    int dir = 0;
+
+    while(total_time < N*N) {
+        if (canMove(dir)) {
+            move(dir);
+            total_time++;
+
+            if (hasEnded())
+                break;
+            
+            if (rightWallExists(dir))
+                continue;
+            
+            dir = turnRigiht(dir);
+            move(dir);
+            total_time++;
+        }
+        else {
+            dir = turnLeft(dir);
+        }
+    }
+
+    if (total_time >= N*N)
+        cout << -1;
+    else 
+        cout << total_time;
+
+
+    return 0;
+}

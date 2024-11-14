06028330f714a704acdcfe8bb8449840a70837d7
homedory <77291306+homedory@users.noreply.github.com>
2024-11-14 23:09:17 +0900

    Title: 정수 사각형 최대 합, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241114/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 105
+
+int dp[MAX_N][MAX_N];
+int N;
+
+void initialize() {
+    for (int i = 2; i <= N; i++) {
+        dp[i][1] = dp[i-1][1] + dp[i][1];
+        dp[1][i] = dp[1][i-1] + dp[1][i];  
+    }
+}
+
+int main() {
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> dp[i][j];
+        }
+    }
+
+    initialize();
+
+    for (int i = 2; i <= N; i++) {
+        for (int j = 2; j <= N; j++) {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + dp[i][j];
+        }
+    }
+
+    cout << dp[N][N];
+
+    
+    return 0;
+}

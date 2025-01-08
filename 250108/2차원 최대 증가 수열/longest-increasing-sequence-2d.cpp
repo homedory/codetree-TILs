63ec490f82dd31201d450886b5421e90bf241477
homedory <77291306+homedory@users.noreply.github.com>
2025-01-08 09:45:30 +0900

    Title: 2차원 최대 증가 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250108/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 250108/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -18,23 +18,27 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            for (int x = 0; x <= i; x++) {
-                for (int y = 0; y <= j; y++) {
+            for (int x = 0; x < i; x++) {
+                for (int y = 0; y < j; y++) {
                     if (dp[x][y] == 0)
                         continue;
 
                     if (x < i && y < j && grid[i][j] > grid[x][y]) 
                         dp[i][j] = max(dp[x][y] + 1, dp[i][j]);
-            
-                    else if (x == i || y == j) 
-                        dp[i][j] = max(dp[x][y], dp[i][j]);
-                    
                 }
             }
         }
     }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            ans = max(ans, dp[i][j]);
+        }
+    }
     
-    cout << dp[n-1][m-1];
+    cout << ans;
 
     return 0;
 }

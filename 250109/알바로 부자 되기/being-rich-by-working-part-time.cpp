12515bf5503fd763ed1b820a7d1d9db877c19a05
homedory <77291306+homedory@users.noreply.github.com>
2025-01-09 15:13:05 +0900

    Title: 알바로 부자 되기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250109/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 250109/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N;
-int s[1000], e[1000], p[1000];
-int dp[1000];
+int s[1005], e[1005], p[1005];
+int dp[1005];
+vector<pair<int,int>> sorted;
 
 int main() {
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         cin >> s[i] >> e[i] >> p[i];
+        sorted.push_back({e[i], i});
     }
 
-    for (int i = 0; i < N; i++) {
-        dp[i] = p[i];
-        for (int j = 0; j < i; j++) {
-            if (e[j] >= s[i])
-                continue;
+    sort(sorted.begin(), sorted.end());
 
-            dp[i] = max(dp[j] + p[i], dp[i]);
+    int max_j = 0;
+    int j = 0;
+    
+    for (int i = 1; i <= N; i++) {
+        while (sorted[j].first < s[i]) {
+            if (dp[max_j] < dp[sorted[j].second])
+                max_j = sorted[j].second;
+            j++;
         }
+
+        dp[i] = max(dp[max_j] + p[i], dp[i]);
     }
 
     int ans = 0;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i <= N; i++) {
         ans = max(ans, dp[i]);
     }
 

e07a063e8633ab107b3a36bfe87b3fa68c4e5bb3
homedory <77291306+homedory@users.noreply.github.com>
2025-02-13 20:49:33 +0900

    Title: 배낭 채우기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250213/배낭 채우기 2/knapsack-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int w[100], v[100];
+int dp[10005];
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> w[i] >> v[i];
+    }
+
+    for (int i = 1; i <= M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i < w[j])
+                continue;
+            
+            dp[i] = max(dp[i], dp[i-w[j]] + v[j]);
+        }
+    }
+
+    cout << dp[M];
+
+    return 0;
+}

d57810ab888caf5aec74d511149d4c0a11d89727
Jinhyong Kim <77291306+homedory@users.noreply.github.com>
2025-04-02 21:27:47 +0900

    Title: 둘 중 하나 잘 고르기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250402/둘 중 하나 잘 고르기/choose-one-of-two-points.cpp
+++ 250402/둘 중 하나 잘 고르기/choose-one-of-two-points.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-#define MAX_LEN 200
-#define MAX_N 100
+#define MAX_LEN 205
+#define MAX_N 105
 
 int main() {
     int N;
     int red_card[MAX_LEN], blue_card[MAX_LEN];
-    int dp[MAX_N] = { 0 };
+    int dp[MAX_N];
+
+
+    dp[0] = 0;
+    for (int i = 1; i <= N; ++i) {
+        dp[i] = INT_MIN;
+    }
+
 
     cin >> N;
     
-    int len = 2 * N;
+    int len = N << 1;
     for (int i = 0; i < len; ++i) {
         cin >> red_card[i] >> blue_card[i];
     }

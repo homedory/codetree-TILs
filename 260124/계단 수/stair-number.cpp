b0de38755283aefbbab6959fdfc2f00fa602e474
Jinhyong Kim <77291306+homedory@users.noreply.github.com>
2026-01-24 01:24:34 +0900

    Title: 계단 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260124/계단 수/stair-number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 1000
+
+int dp[MAX_N + 1][10];
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= 9; i++) {
+        dp[1][i] = 1;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        dp[i][0] += dp[i-1][1];
+        dp[i][9] += dp[i-1][8];
+
+        for (int j = 1; j <= 8; j++) {
+            dp[i][j] += dp[i-1][j+1];
+            dp[i][j] += dp[i-1][j-1];
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i <= 9; i++) {
+        ans += dp[N][i];
+    }
+
+    cout << ans;
+
+    return 0;
+}
